4c02805c818d4eb814cfb82ed22b1db28b3c2d77
Chirag Gupta <chirag4522gupta@gmail.com>
2025-12-30 18:26:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+const timer = document.createElement("p");
+timer.id = "timer";
+document.body.appendChild(timer);
+
+
+function updateTimer() {
+  const now = new Date();
+  timer.textContent = now.toLocaleString();
+}
+
+
+updateTimer();
+
+// update every second
+setInterval(updateTimer, 1000);
